e8dea9310f7afd5f0dfa7c97b58ffa00d3cdd3d2
Jake David <jakedgmu13@gmail.com>
2019-07-25 17:46:27 -0400

    added images to public folder, created data.json with image states, added some components.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">
+        Made with React and Create React App
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>A game of clicks</h1>
+      <h2>Click an image once to get points, click on the same image twice and its game over!</h2>
+    </header>
+  );
+}
+
+export default Header;
